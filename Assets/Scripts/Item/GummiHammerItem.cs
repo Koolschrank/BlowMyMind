0a64c44805a53ab8935c991eca8948108c72f56b
G0vinda <frederickfreund@gmail.com>
2024-01-27 19:27:10 +0100

    Add GummiHammer

---
+++ Assets/Scripts/Item/GummiHammerItem.cs
@@ -0,0 +1,42 @@
+using Player;
+using UnityEngine;
+
+namespace Item
+{
+    public class GummiHammerItem : Item
+    {
+        [SerializeField] private HitData hitData;
+        [SerializeField] private float coolDownTime;
+        
+        public override void Initialize(PlayerCharacter player)
+        {
+            Player = player;
+        }
+
+        public override void Use()
+        {
+            Invoke(nameof(FinishUse), coolDownTime);
+        }
+
+        public override void Impact(Collider collider)
+        {
+            if (!collider.TryGetComponent(out PlayerCharacter nearbyPlayer))
+                return;
+                
+            if(nearbyPlayer == Player)
+                return;
+                
+            Vector3  power = transform.forward * hitData.ForwardForce + transform.up * hitData.UpForce;
+            nearbyPlayer.TakeDamage(power, hitData);
+                
+            hitData.ActivateEffects();
+        }
+        
+        private void FinishUse()
+        {
+            InUse = false;
+            UseCompleted?.Invoke();
+            Depleted?.Invoke();
+        }
+    }
+}
