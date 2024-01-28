9a57862df6a083c9eb7b95eb19f28ca04920a594
bene-labs <62158116+bene-labs@users.noreply.github.com>
2024-01-28 14:45:35 +0100

    add Death Particles

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -41,6 +41,7 @@ namespace Player
         public DamageNumberValue damageNumberValue;
         [SerializeField] private LaughParticles laughParticles;
         [SerializeField] private ParticleSystemRenderer laughRender;
+        [SerializeField] private DeathParticles deathParticles;
         
         public float groundedDrag = 1f;
         public float airDrag = 0f;
@@ -328,6 +329,9 @@ namespace Player
         public void Die()
         {
             damage.Value = damage.Value_min;
+            var deathPosition = transform.position;
+            var newDeathParticles = Instantiate(deathParticles, deathPosition, Quaternion.Euler(-90, 0, 0));
+            newDeathParticles.GetComponent<DeathParticles>().SetColor(color);
             Debug.Log("Player died!");
             // destroy game object
             PlayerSystem.instance.Respawn(gameObject);
@@ -335,11 +339,10 @@ namespace Player
             {
                 SetFaceMaterials(availableFaces[Random.Range(0, availableFaces.Count)]);
             }
+
             rigidBody.velocity = Vector3.zero;
             lives.Value -= 1f;
-
             PlayerSystem.instance.CheckForWinner();
-
         }
     }
 }
