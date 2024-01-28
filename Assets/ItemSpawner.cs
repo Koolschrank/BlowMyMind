245bbed69c50e8e5188531e728a29c9569eb487d
G0vinda <frederickfreund@gmail.com>
2024-01-28 12:42:42 +0100

    Add probabilities to itemSpawner

--- Assets/ItemSpawner.cs
+++ Assets/ItemSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using SystemScripts;
 using UnityEngine;
 
@@ -7,14 +8,28 @@ public class ItemSpawner : MonoBehaviour
 {
     public float spawnTime = 5f;
     public GameObject[] items;
-    float spawnTimeCounter = 0f;
+    public float[] probabilities;
 
-    public Vector3 minPos = new Vector3(-5f, 10f, -5f);
-    public Vector3 maxPos = new Vector3(5f, 10f, 5f);
-    // Start is called before the first frame update
+    public Vector3 minPos = new (-5f, 10f, -5f);
+    public Vector3 maxPos = new (5f, 10f, 5f);
+    
+    private float _spawnTimeCounter = 0f;
+    private float _probabilitySum;
+    private List<(float, ProbabilitySet)> _probabilitySets = new ();
+    
     void Start()
     {
-        
+        if(items.Length != probabilities.Length)
+            Debug.LogError("ItemSpawner: number of items and probabilities do not match!");
+
+        _probabilitySum = probabilities.Sum();
+        var probabilityVal = 0f;
+        for (var i = 0; i < probabilities.Length; i++)
+        {
+            var endProbability = probabilityVal + probabilities[i];
+            _probabilitySets.Add((probabilityVal, new ProbabilitySet(endProbability, items[i])));
+            probabilityVal = endProbability;
+        }
     }
 
     // Update is called once per frame
@@ -26,10 +41,10 @@ public class ItemSpawner : MonoBehaviour
         }
         
         // spawn item
-        spawnTimeCounter += Time.deltaTime;
-        if (spawnTimeCounter >= spawnTime)
+        _spawnTimeCounter += Time.deltaTime;
+        if (_spawnTimeCounter >= spawnTime)
         {
-            spawnTimeCounter = 0f;
+            _spawnTimeCounter = 0f;
             SpawnItem();
         }
         
@@ -37,11 +52,40 @@ public class ItemSpawner : MonoBehaviour
 
     void SpawnItem()
     {
+        var randomVal = Random.Range(0, _probabilitySum);
+        GameObject pickupItem = null;
+        for (var i = 0; i < _probabilitySets.Count; i++)
+        {
+            if (_probabilitySets[i].Item1 <= randomVal)
+            {
+                if (_probabilitySets[i].Item2.EndProbability > randomVal)
+                {
+                    pickupItem = _probabilitySets[i].Item2.Item;
+                }
+            }
+        }
+        
+        if(pickupItem == null)
+        {
+            Debug.LogError("Error on random item generation");
+            return;
+        }
+        
         // random position
         Vector3 pos = new Vector3(Random.Range(minPos.x, maxPos.x), Random.Range(minPos.y, maxPos.y), Random.Range(minPos.z, maxPos.z));
-        // random item
-        GameObject item = items[Random.Range(0, items.Length)];
         // spawn item
-        Instantiate(item, pos, Quaternion.identity);
+        Instantiate(pickupItem, pos, Quaternion.identity);
+    }
+
+    private struct ProbabilitySet
+    {
+        public float EndProbability;
+        public GameObject Item;
+
+        public ProbabilitySet(float endProbability, GameObject item)
+        {
+            EndProbability = endProbability;
+            Item = item;
+        }
     }
 }
